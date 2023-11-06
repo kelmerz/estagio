a910c34430a298294085c1e43536516906c480d4
kelmerz <149977389+kelmerz@users.noreply.github.com>
2023-11-05 22:02:36 -0300

    Update veiculos.js

--- veiculos.js
+++ veiculos.js
@@ -1,1 +1,41 @@
+import axios from 'axios';
 
+class Veiculo {
+  constructor(locadora, modelo, marca, ano, motor, portas, cambio, arCondicionado) {
+    this.locadora = locadora;
+    this.modelo = modelo;
+    this.marca = marca;
+    this.ano = ano;
+    this.motor = motor;
+    this.portas = portas;
+    this.cambio = cambio;
+    this.arCondicionado = arCondicionado;
+  }
+}
+function saveVeiculo(veiculo) {
+  axios.post('/veiculos', veiculo)
+    .then(response => {
+      console.log('Veículo salvo com sucesso!');
+    })
+    .catch(error => {
+      console.error('Erro ao salvar veículo:', error);
+    });
+}
+
+function fetchVeiculos() {
+  axios.get('/veiculos')
+    .then(response => {
+      const veiculos = response.data;
+      console.log('Veículos recuperados com sucesso!');
+      console.log(veiculos);
+    })
+    .catch(error => {
+      console.error('Erro ao buscar veículos:', error);
+    });
+}
+
+const veiculo = new Veiculo('Locadora XYZ', 'Modelo ABC', 'Marca 123', 2023, '1.0L', 4, 'Automático', true);
+saveVeiculo(veiculo);
+
+// Usage example
+fetchVeiculos();
